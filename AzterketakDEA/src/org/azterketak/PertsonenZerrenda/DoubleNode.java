c5ed26b8cce06f732e39b197ae9c871fffac750b
yassin <yassin@mkyassin>
2025-01-25 16:07:57 +0100

    Proba VIM

--- AzterketakDEA/src/org/azterketak/PertsonenZerrenda/DoubleNode.java
+++ AzterketakDEA/src/org/azterketak/PertsonenZerrenda/DoubleNode.java
@@ -5,4 +5,19 @@ public class DoubleNode{
 	DoubleNode prev;
 	DoubleNode next;
 	Node erosketak; //Zerrenda zirkularra
+	
+	public void zerrendaSortu(String[] erosketak1){
+		if(erosketak==null){
+			erosketak = new Node();
+			erosketak.data = erosketak1[0];
+		}
+		Node lag = erosketak;
+		for(int i=1; i<erosketak1.length;i++){
+			Node berria = new Node();
+			berria.data = erosketak1[1];
+			lag.next = berria;
+			lag = lag.next;
+		}
+		lag.next = erosketak;
+	}
 }
