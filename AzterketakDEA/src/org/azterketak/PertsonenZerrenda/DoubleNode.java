155d7447229f5af8ed097b7bf852267c485b54f3
yassin <yassin@mkyassin>
2025-01-25 12:11:54 +0100

    Prueba VIM

---
+++ AzterketakDEA/src/org/azterketak/PertsonenZerrenda/DoubleNode.java
@@ -0,0 +1,8 @@
+package org.azterketak.PertsonenZerrenda
+
+public class DoubleNode{
+	String data;
+	DoubleNode prev;
+	DoubleNode next;
+	Node erosketak; //Zerrenda zirkularra
+}
---
+++ AzterketakDEA/src/org/azterketak/PertsonenZerrenda/PertsonenZerrenda.java
@@ -0,0 +1,6 @@
+package org.azterketak.PertsonenZerrenda
+
+public class PertsonenZerrenda{
+	String[] pertsonak;
+	String[][] erosketak;
+}
